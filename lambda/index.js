e6b25c3718f1d7610774a9a88150ac5ea74e94e1
Patrick Carawan <patrickcarawan@gmail.com>
2025-02-17 17:15:48 -0800

    Lamba function working and cleanup

--- lambda/index.js
+++ lambda/index.js
@@ -1,23 +1,29 @@
-const base64url = require('base64url');
 const zlib = require('zlib');
 
 exports.handler = async function (event, context) {
-  
-// 	console.log("HELLO event query params", event.queryParameters);
-    console.log("HEEYYYY event", {event})
-	const decoded = base64url.decode(event.queryStringParameters.file)
-// 	console.log(decoded)
-    const buffer = new Buffer(decoded);
-    const output = zlib.inflate(buffer);
-    console.log("OUTPUT", output)
+    // Get Base64Url string and convert to Base64
+    let file = event.queryStringParameters.file
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+    while (file.length % 4 > 0) {
+        file += '=';
+    }
+    // Decode from Base64 into chars
+    const decoded = atob(file);
+    // Convert chars into char codes
+    let charCodes = new Uint8Array(decoded.length);
+    for (let i = 0; i < decoded.length; i++) {
+        charCodes[i] = decoded.charCodeAt(i);
+    }
+    // Inflate
+    const inflated = zlib.inflateRawSync(charCodes).toString();
     return {
         'statusCode': 200,
-		'body': `<svg xmlns="http://www.w3.org/2000/svg" height="200" width="350"><path id="lineAC" d="M 30 180 q 150 -250 300 0" stroke="blue" stroke-width="2" fill="none"></path><text style="fill:red;font-size:25px;"><textPath href="#lineAC">I love SVG! I love SVG!</textPath></text>
-  Sorry, your browser does not support inline SVG.
-</svg>`,
+        'body': inflated,
         'headers': {
             'Content-Type': 'image/svg+xml'
 	    }
     }
 };
+
 
--- lambda/todo.md
+++
@@ -1,1 +0,0 @@
-Figure out why hitting the lambda function with a long url causes it to return "Message null"
